42f42721f7c762194f6910a75d56c27af351c331
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2021-02-20 21:18:55 +0100

    Post model and listing out all post

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Posts extends Component
 {
+    public $posts;
+
+    public function mount() {
+        $this->posts = Post::latest()->take(100)->get();
+    }
+
     public function render()
     {
         return view('livewire.posts');
--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,22 @@
 <div>
-    You have login
+    @foreach($posts as $post)
+        <div class="flex items-center justify-between borrder-b border-gray-200 p-3">
+            <div class="flex">
+                <img src="..." alt="..." class="w-12 h-12 rounded-full mr-4">
+                <div>
+                    <h2 class="font-semibold">
+                        {{ $post->user->name }}
+                        <span class="text-sm text-gray-500">
+                            {{ $post->created_at->diffForHumans() }}
+                        </span>
+                    </h2>
+                    <p class="mb-2">{{ $post->body }}</p>
+                </div>
+            </div>
+            <div>
+                Likes
+            </div>
+        </div>
+
+    @endforeach
 </div>
