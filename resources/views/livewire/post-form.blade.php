9f3f8f17d4548d16d6490feb3f9f97ec0df4d1ba
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2021-02-20 21:46:57 +0100

    post form view

---
+++ resources/views/livewire/post-form.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <form action="#">
+        <div class="mb-3">
+            <label for="body" class="sr-only">Post Body</label>
+            <textarea name="body" id="body" cols="30" rows="3" class="w-full border-2
+             border-gray-200 rounded-lg" placeholder="Share something.."></textarea>
+        </div>
+
+        <button type="submit"
+            class="h-10 px-4 text-center text-white bg-indigo-500 font-medium rounded-lg">
+            Post it
+        </button>
+    </form>
+</div>
